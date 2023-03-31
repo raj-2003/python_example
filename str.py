adabd67ad0394e3332979df2dbc0999faa9f9e9c
barotraj2909 <rajbarot932847@gmail.com>
2023-03-31 22:58:41 +0530

    31.03.2023

---
+++ str.py
@@ -0,0 +1,13 @@
+s="Java programing"
+
+print(s[1:13])
+print(s[:15])
+print(s[5:])
+print(s[2:16:3])
+print(s[::15])
+
+print(s[-15:-5])
+print(s[:-3])
+print(s[-13:])
+print(s[-16:-1:-4])
+print(s[::-2])
